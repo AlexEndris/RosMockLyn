b3df5a03b1992ab5431d7c4d7790d5107c595618
Alexander <qudeid@alexendris.com>
2015-06-04 16:17:50 +0200

    Updated version

--- RosMockLyn.Mocking/Properties/AssemblyInfo.cs
+++ RosMockLyn.Mocking/Properties/AssemblyInfo.cs
@@ -33,11 +33,11 @@ using System.Runtime.CompilerServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RosMockLyn.Mocking")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The mocking framework.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RosMockLyn.Mocking")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © 2014, 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -52,7 +52,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
 
 [assembly: InternalsVisibleTo("RosMockLyn.Mocking.Tests")]
